dafbac8538e391c6735ee278e89b559c07887c42
Steve Yen <steve.yen@gmail.com>
2009-04-02 20:33:42 -0700

    t/cproxy_one.pl now even uglier perl, but works

--- t/cproxy_one.pl
+++ t/cproxy_one.pl
@@ -9,13 +9,76 @@ use MemcachedTest;
 
 *new_memcached_orig = *new_memcached;
 
+use IO::Socket::INET;
+use IO::Socket::UNIX;
+use Exporter 'import';
+use Carp qw(croak);
+use vars qw(@EXPORT);
+
+use Cwd;
+my $builddir = getcwd;
+
 sub new_memcached_proxy {
-  my ($args, $passed_port) = @_;
-  my $portA = $passed_port || free_port();
-  my $portB = free_port();
-  $args .= " -W $portA=localhost:$portB -p $portB";
-  print("new_memcached_proxy $args\n");
-  return new_memcached_orig($args, $portA);
+    my ($args, $passed_port) = @_;
+    my $port  = $passed_port || free_port();
+    my $portB = free_port();
+    my $portC = $portB;
+
+    $args .= " -W $port=localhost:$portB -p $portC";
+
+    print("new_memcached_proxy $args\n");
+
+    my $udpport = free_port("udp");
+    if (supports_udp()) {
+        $args .= " -U $udpport";
+    }
+    if ($< == 0) {
+        $args .= " -u root";
+    }
+    my $childpid = fork();
+
+    my $exe = "$builddir/memcached-debug";
+    croak("memcached binary doesn't exist.  Haven't run 'make' ?\n") unless -e $exe;
+    croak("memcached binary not executable\n") unless -x _;
+
+    unless ($childpid) {
+        exec "$exe $args";
+        exit; # never gets here.
+    }
+
+    # unix domain sockets
+    if ($args =~ /-s (\S+)/) {
+        sleep 1;
+	my $filename = $1;
+	my $conn = IO::Socket::UNIX->new(Peer => $filename) ||
+	    croak("Failed to connect to unix domain socket: $! '$filename'");
+
+	return Memcached::Handle->new(pid  => $childpid,
+				      conn => $conn,
+				      domainsocket => $filename,
+				      port => $port);
+    }
+
+    # try to connect / find open port, only if we're not using unix domain
+    # sockets
+
+    for (1..20) {
+	my $conn = IO::Socket::INET->new(PeerAddr => "127.0.0.1:$port");
+	if ($conn) {
+	    return Memcached::Handle->new(pid  => $childpid,
+					  conn => $conn,
+					  udpport => $udpport,
+					  port => $port);
+	}
+	select undef, undef, undef, 0.10;
+    }
+    croak("Failed to startup/connect to memcached server.");
+}
+
+sub supports_udp {
+    my $output = `$builddir/memcached-debug -h`;
+    return 0 if $output =~ /^memcached 1\.1\./;
+    return 1;
 }
 
 *new_memcached = *new_memcached_proxy;
@@ -38,5 +101,6 @@ if ($test_name =~ /cproxy/) {
   print("fail cannot test against self\n");
 } else {
   eval($prefix . `cat $test_name`);
+  print($@);
 }
 
