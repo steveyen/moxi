1bfa0de31a1be440cbe6c554c6b93f7427ca33cd
Steve Yen <steve.yen@gmail.com>
2009-04-23 19:31:54 -0700

    comments on where t/moxi.pl fails

--- t/moxi.pl
+++ t/moxi.pl
@@ -5,37 +5,50 @@ use strict;
 my $topology_name = $ARGV[0] || 'simple';
 
 my @good_tests = qw(
-binary-get.t
-bogus-commands.t
-cas.t
-expirations.t
-flags.t
-flush-all.t
-incrdecr.t
-line-lengths.t
-maxconns.t
-multiversioning.t
-slab-reassign.t
+  binary-get.t
+  bogus-commands.t
+  cas.t
+  expirations.t
+  flags.t
+  flush-all.t
+  incrdecr.t
+  line-lengths.t
+  maxconns.t
+  multiversioning.t
+  slab-reassign.t
 );
 
 my @skip_tests = qw(
-noreply.t
-getset.t
+  # TODO: The noreply test fails because we use concurrent
+  # downstream connections rather than respecting
+  # the serial expectations of the noreply test.
+  #
+  noreply.t
 
-00-startup.t
-64bit.t
-binary.t
-daemonize.t
-dash-M.t
-issue_14.t
-issue_22.t
-issue_29.t
-lru.t
-stats-detail.t
-stats.t
-udp.t
-unixsocket.t
-whitespace.t
+  # TODO: The getset test fails because the failed set
+  # of a huge >1MB value does not delete the item from
+  # the downstream memcached server.
+  #
+  getset.t
+
+  # The following tests are not expected to succeed,
+  # because they're testing command line stuff,
+  # or the stats are wrong.
+  #
+  00-startup.t
+  64bit.t
+  binary.t
+  daemonize.t
+  dash-M.t
+  issue_14.t
+  issue_22.t
+  issue_29.t
+  lru.t
+  stats-detail.t
+  stats.t
+  udp.t
+  unixsocket.t
+  whitespace.t
 );
 
 my %is_good_test;
