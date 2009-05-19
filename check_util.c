a35da8b1e775b867db74bb87cff938f4b12ab2c7
Dustin Sallings <dustin@spy.net>
2009-05-19 11:58:33 -0700

    Converted util tests to check.

--- check_util.c
+++ check_util.c
@@ -6,9 +6,117 @@
 
 #include "memcached.h"
 
-START_TEST (test_one)
+START_TEST (test_safe_strtoul)
 {
-    fail_if(false, "False has no truth.");
+    uint32_t val;
+    fail_unless(safe_strtoul("123", &val), "Failed parsing 123");
+    fail_unless(val == 123, "Didn't parse 123 to 123");
+    fail_unless(safe_strtoul("+123", &val), "Failed parsing +123");
+    fail_unless(val == 123, "Didn't parse +123 to 123");
+    fail_if(safe_strtoul("", &val), "Unexpectedly parsed an empty string.");
+    fail_if(safe_strtoul("123BOGUS", &val), "Parsed 123BOGUS");
+    /* Not sure what it does, but this works with ICC :/
+       assert(!safe_strtoul("92837498237498237498029383", &val)); // out of range
+    */
+
+    // extremes:
+    // 2**32 -1
+    fail_unless(safe_strtoul("4294967295", &val), "Failed parsing 4294967295");
+    fail_unless(val == 4294967295L, "4294967295 != 4294967295L somehow");
+    /* This actually works on 64-bit ubuntu
+       assert(!safe_strtoul("4294967296", &val)); // 2**32
+    */
+    fail_if(safe_strtoul("-1", &val), "Parsed a negative number as unsigned.");
+}
+END_TEST
+
+START_TEST (test_safe_strtoull)
+{
+    uint64_t val;
+
+    fail_unless(safe_strtoull("123", &val), "Failed parsing 123");
+    fail_unless(val == 123, "Didn't parse 123 to 123");
+    fail_unless(safe_strtoull("+123", &val), "Failed parsing +123");
+    fail_unless(val == 123, "Didn't parse +123 to 123");
+    fail_if(safe_strtoull("", &val), "Unexpectedly parsed an empty string.");
+    fail_if(safe_strtoull("123BOGUS", &val), "Parsed 123BOGUS");
+
+    fail_if(safe_strtoull("92837498237498237498029383", &val),
+            "Parsed out of range value.");
+
+    // extremes:
+    fail_unless(safe_strtoull("18446744073709551615", &val),
+                "Failed parsing 18446744073709551615"); // 2**64 - 1
+    fail_unless(val == 18446744073709551615ULL,
+                "18446744073709551615 != 18446744073709551615ULL");
+    fail_if(safe_strtoull("18446744073709551616", &val),
+            "Parsed 18446744073709551616"); // 2**64
+    fail_if(safe_strtoull("-1", &val), "Parsed a negative number as unsigned.");
+}
+END_TEST
+
+START_TEST (test_safe_strtoll)
+{
+    int64_t val;
+    fail_unless(safe_strtoll("123", &val), "Failed parsing 123");
+    fail_unless(val == 123, "123 != 123");
+    fail_unless(safe_strtoll("+123", &val), "Failed parsing +123");
+    fail_unless(val == 123, "123 != 123");
+    fail_unless(safe_strtoll("-123", &val), "Failed parsing -123");
+    fail_unless(val == -123, "-123 != -123");
+    fail_if(safe_strtoll("", &val), "Parsed an empty string"); // empty
+    fail_if(safe_strtoll("123BOGUS", &val), "Parsed 123BOGUS"); // non-numeric
+    fail_if(safe_strtoll("92837498237498237498029383", &val),
+            "Parsed out of range value"); // out of range
+
+    // extremes:
+    fail_if(safe_strtoll("18446744073709551615", &val),
+            "Parsed out of range value"); // 2**64 - 1
+    fail_unless(safe_strtoll("9223372036854775807", &val),
+                "Failed parsing 9223372036854775807"); // 2**63 - 1
+    fail_unless(val == 9223372036854775807LL,
+                "9223372036854775807 != 9223372036854775807LL");
+    /*
+      assert(safe_strtoll("-9223372036854775808", &val)); // -2**63
+      assert(val == -9223372036854775808LL);
+    */
+    fail_if(safe_strtoll("-9223372036854775809", &val),
+            "Parsed out of range value"); // -2**63 - 1
+
+    // We'll allow space to terminate the string.  And leading space.
+    fail_unless(safe_strtoll(" 123 foo", &val), "Failed parsing \" 123 foo\"");
+    fail_unless(val == 123, "\" 123 foo\" != 123");
+}
+END_TEST
+
+START_TEST (test_safe_strtol)
+{
+    int32_t val;
+    fail_unless(safe_strtol("123", &val), "Failed parsing 123");
+    fail_unless(val == 123, "123 != 123");
+    fail_unless(safe_strtol("+123", &val), "Failed parsing +123");
+    fail_unless(val == 123, "+123 != 123");
+    fail_unless(safe_strtol("-123", &val), "Failed parsing -123");
+    fail_unless(val == -123, "-123 != -123");
+    fail_if(safe_strtol("", &val), "Parsing empty string");
+    fail_if(safe_strtol("123BOGUS", &val), "Parsed 123BOGUS");
+    fail_if(safe_strtol("92837498237498237498029383", &val),
+            "Parsed out of range value.");
+
+    // extremes:
+    /* This actually works on 64-bit ubuntu
+       assert(!safe_strtol("2147483648", &val)); // (expt 2.0 31.0)
+    */
+    fail_unless(safe_strtol("2147483647", &val),
+                "Failed parsing upper limit."); // (- (expt 2.0 31) 1)
+    fail_unless(val == 2147483647L, "2147483647 != 2147483647L");
+    /* This actually works on 64-bit ubuntu
+       assert(!safe_strtol("-2147483649", &val)); // (- (expt -2.0 31) 1)
+    */
+
+    // We'll allow space to terminate the string.  And leading space.
+    fail_unless(safe_strtol(" 123 foo", &val), "Failed parsing \" 123 foo\"");
+    fail_unless(val == 123, "\" 123 foo\" != 123");
 }
 END_TEST
 
@@ -18,7 +126,10 @@ static Suite* util_suite (void)
 
     /* Core test case */
     TCase *tc_core = tcase_create ("Core");
-    tcase_add_test(tc_core, test_one);
+    tcase_add_test(tc_core, test_safe_strtoul);
+    tcase_add_test(tc_core, test_safe_strtoull);
+    tcase_add_test(tc_core, test_safe_strtoll);
+    tcase_add_test(tc_core, test_safe_strtol);
     suite_add_tcase(s, tc_core);
 
     return s;
--- testapp.c
+++ testapp.c
@@ -133,103 +133,6 @@ static enum test_return cache_redzone_test(void)
 #endif
 }
 
-static enum test_return test_safe_strtoul(void) {
-    uint32_t val;
-    assert(safe_strtoul("123", &val));
-    assert(val == 123);
-    assert(safe_strtoul("+123", &val));
-    assert(val == 123);
-    assert(!safe_strtoul("", &val));  // empty
-    assert(!safe_strtoul("123BOGUS", &val));  // non-numeric
-    /* Not sure what it does, but this works with ICC :/
-       assert(!safe_strtoul("92837498237498237498029383", &val)); // out of range
-    */
-
-    // extremes:
-    assert(safe_strtoul("4294967295", &val)); // 2**32 - 1
-    assert(val == 4294967295L);
-    /* This actually works on 64-bit ubuntu
-       assert(!safe_strtoul("4294967296", &val)); // 2**32
-    */
-    assert(!safe_strtoul("-1", &val));  // negative
-    return TEST_PASS;
-}
-
-
-static enum test_return test_safe_strtoull(void) {
-    uint64_t val;
-    assert(safe_strtoull("123", &val));
-    assert(val == 123);
-    assert(safe_strtoull("+123", &val));
-    assert(val == 123);
-    assert(!safe_strtoull("", &val));  // empty
-    assert(!safe_strtoull("123BOGUS", &val));  // non-numeric
-    assert(!safe_strtoull("92837498237498237498029383", &val)); // out of range
-
-    // extremes:
-    assert(safe_strtoull("18446744073709551615", &val)); // 2**64 - 1
-    assert(val == 18446744073709551615ULL);
-    assert(!safe_strtoull("18446744073709551616", &val)); // 2**64
-    assert(!safe_strtoull("-1", &val));  // negative
-    return TEST_PASS;
-}
-
-static enum test_return test_safe_strtoll(void) {
-    int64_t val;
-    assert(safe_strtoll("123", &val));
-    assert(val == 123);
-    assert(safe_strtoll("+123", &val));
-    assert(val == 123);
-    assert(safe_strtoll("-123", &val));
-    assert(val == -123);
-    assert(!safe_strtoll("", &val));  // empty
-    assert(!safe_strtoll("123BOGUS", &val));  // non-numeric
-    assert(!safe_strtoll("92837498237498237498029383", &val)); // out of range
-
-    // extremes:
-    assert(!safe_strtoll("18446744073709551615", &val)); // 2**64 - 1
-    assert(safe_strtoll("9223372036854775807", &val)); // 2**63 - 1
-    assert(val == 9223372036854775807LL);
-    /*
-      assert(safe_strtoll("-9223372036854775808", &val)); // -2**63
-      assert(val == -9223372036854775808LL);
-    */
-    assert(!safe_strtoll("-9223372036854775809", &val)); // -2**63 - 1
-
-    // We'll allow space to terminate the string.  And leading space.
-    assert(safe_strtoll(" 123 foo", &val));
-    assert(val == 123);
-    return TEST_PASS;
-}
-
-static enum test_return test_safe_strtol(void) {
-    int32_t val;
-    assert(safe_strtol("123", &val));
-    assert(val == 123);
-    assert(safe_strtol("+123", &val));
-    assert(val == 123);
-    assert(safe_strtol("-123", &val));
-    assert(val == -123);
-    assert(!safe_strtol("", &val));  // empty
-    assert(!safe_strtol("123BOGUS", &val));  // non-numeric
-    assert(!safe_strtol("92837498237498237498029383", &val)); // out of range
-
-    // extremes:
-    /* This actually works on 64-bit ubuntu
-       assert(!safe_strtol("2147483648", &val)); // (expt 2.0 31.0)
-    */
-    assert(safe_strtol("2147483647", &val)); // (- (expt 2.0 31) 1)
-    assert(val == 2147483647L);
-    /* This actually works on 64-bit ubuntu
-       assert(!safe_strtol("-2147483649", &val)); // (- (expt -2.0 31) 1)
-    */
-
-    // We'll allow space to terminate the string.  And leading space.
-    assert(safe_strtol(" 123 foo", &val));
-    assert(val == 123);
-    return TEST_PASS;
-}
-
 static enum test_return test_issue_44(void) {
     char pidfile[80];
     char buffer[256];
@@ -252,8 +155,6 @@ static enum test_return test_issue_44(void) {
     return TEST_PASS;
 }
 
-
-
 typedef enum test_return (*TEST_FUNC)(void);
 struct testcase {
     const char *description;
@@ -267,10 +168,6 @@ struct testcase testcases[] = {
     { "cache_destructor", cache_destructor_test },
     { "cache_reuse", cache_reuse_test },
     { "cache_redzone", cache_redzone_test },
-    { "strtol", test_safe_strtol },
-    { "strtoll", test_safe_strtoll },
-    { "strtoul", test_safe_strtoul },
-    { "strtoull", test_safe_strtoull },
     { "issue_44", test_issue_44 },
     { NULL, NULL }
 };
