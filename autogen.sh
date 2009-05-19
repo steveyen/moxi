a424e1b76776922b85dae150513020ba564a3ff9
Matt Ingenthron <ingenthr@cep.net>
2009-05-19 13:07:47 -0700

    Fixed some broken autogen.sh error handling and added MacOS defaults

--- autogen.sh
+++ autogen.sh
@@ -10,17 +10,24 @@
 # Get the initial version.
 sh version.sh
 
+# check for sane defaults on some platforms
+UNAME=`uname -s`
+if [ -d /opt/local/share/aclocal ] && [ $UNAME = "Darwin" ] ; then
+ACLOCALFLAGS=" -I /opt/local/share/aclocal"
+fi
+
+
 echo "aclocal..."
-ACLOCAL=`which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal || exit 1`
-$ACLOCAL || exit 1
+ACLOCAL=`which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15`
+${ACLOCAL:-aclocal} $ACLOCALFLAGS || exit 1
 
 echo "autoheader..."
 AUTOHEADER=${AUTOHEADER:-autoheader}
 $AUTOHEADER || exit 1
 
 echo "automake..."
-AUTOMAKE=`which automake-1.10 || which automake-1.9 || which automake-1.7 || exit 1`
-$AUTOMAKE --foreign --add-missing || automake --gnu --add-missing || exit 1
+AUTOMAKE=`which automake-1.10 || which automake-1.9 || which automake-1.7`
+$AUTOMAKE --foreign --add-missing || automake --foreign --add-missing || exit 1
 
 echo "autoconf..."
 AUTOCONF=${AUTOCONF:-autoconf}
