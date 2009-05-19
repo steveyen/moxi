d7b8628b0973ea768476f323ff69cc981e1bceaa
Dustin Sallings <dustin@spy.net>
2009-05-19 15:23:07 -0700

    Move timeval_subtract into util.c

--- util.h
+++ util.h
@@ -11,3 +11,11 @@ bool safe_strtoull(const char *str, uint64_t *out);
 bool safe_strtoll(const char *str, int64_t *out);
 bool safe_strtoul(const char *str, uint32_t *out);
 bool safe_strtol(const char *str, int32_t *out);
+
+/* This was stolen from the glibc docs.
+
+   Why they'd write documentation to show how to do this vs. just
+   provide the function is unclear.
+*/
+int timeval_subtract(struct timeval *result,
+                     struct timeval *x, struct timeval *y);
