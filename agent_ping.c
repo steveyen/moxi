f7ceb53a8bc9cb61c37bd3c4599a77d22d578ef9
Dustin Sallings <dustin@spy.net>
2009-05-20 21:42:26 -0700

    Process management controller specified ping tests.

--- agent_ping.c
+++ agent_ping.c
@@ -17,15 +17,66 @@
 memcached_return memcached_connect(memcached_server_st *ptr);
 memcached_return memcached_version(memcached_st *ptr);
 
-#define DEFAULT_LATENCY_ITERATIONS 5
-
 // Local declarations.
 //
+
+struct ping_test_recipe {
+    char *name;
+    int keysize;
+    int valsize;
+    int iterations;
+};
+
 void ping_server(char *server_name,
-                 int latency_iterations,
+                 struct ping_test_recipe *recipes,
                  proxy_behavior *behavior,
                  void *opaque,
                  conflate_add_ping_report add_report);
+
+static int charlistlen(char **in) {
+    int rv = 0;
+    while (in[rv]) {
+        rv++;
+    }
+    return rv;
+}
+
+static bool starts_with(const char* needle, const char *haystack)
+{
+    return strncmp(needle, haystack, strlen(needle)) == 0;
+}
+
+static void load_ping_recipe(char **params,
+                             struct ping_test_recipe *out)
+{
+    assert(params);
+    assert(out);
+
+    for (int i = 0; params[i]; i++) {
+        int value = 0;
+        char *p = NULL;
+
+        p = strchr(params[i], '=');
+        assert(p);
+
+        if (!safe_strtol(p+1, &value)) {
+            fprintf(stderr, "Failed to parse ``%s'' as number\n", p+1);
+            assert(false);
+        }
+
+        if (starts_with("ksize=", params[i])) {
+            out->keysize = value;
+        } else if (starts_with("vsize=", params[i])) {
+            out->valsize = value;
+        } else if (starts_with("iterations=", params[i])) {
+            out->iterations = value;
+        } else {
+            if (settings.verbose > 1) {
+                fprintf(stderr, "Unknown recipe property:  %s\n", params[i]);
+            }
+        }
+    }
+}
 
 void on_conflate_ping_test(void *userdata, void *opaque,
                            kvpair_t *form,
@@ -50,47 +101,124 @@ void on_conflate_ping_test(void *userdata, void *opaque,
     //    bucket=buck1
     //    usr=test1
     //    pwd=password
-    //
+    //  tests
+    //    test1
+    //    test2
+    //  def-test1
+    //    ksize=16
+    //    vsize=16
+    //    iterations=500
+    //  def-test2
+    //    ksize=64
+    //    vsize=524288
+    //    iterations=50
     if (form != NULL) {
-        char   server_key[200];
-        char **servers = get_key_values(form, "servers");
+        char   detail_key[200];
 
-        int latency_iterations = DEFAULT_LATENCY_ITERATIONS;
-        kvpair_t *l_iter = find_kvpair(form, "latency_iterations");
-        if (l_iter) {
-            if (!safe_strtol(l_iter->values[0], &latency_iterations)) {
-                latency_iterations = DEFAULT_LATENCY_ITERATIONS;
-            }
+        // Discover test configuration.
+        char **tests = get_key_values(form, "tests");
+        int nrecipes = charlistlen(tests);
+        struct ping_test_recipe recipes[nrecipes+1];
+        memset(recipes, 0x00, sizeof(struct ping_test_recipe) * (nrecipes+1));
+        for (int j = 0; j < nrecipes; j++) {
+            snprintf(detail_key, sizeof(detail_key), "def-%s", tests[j]);
+            recipes[j].name = strdup(detail_key);
+            assert(recipes[j].name);
+            load_ping_recipe(get_key_values(form, detail_key), &recipes[j]);
         }
 
+        // Initialize each server and run the tests
+        char **servers = get_key_values(form, "servers");
         for (int j = 0; servers != NULL && servers[j]; j++) {
-            snprintf(server_key, sizeof(server_key),
+            snprintf(detail_key, sizeof(detail_key),
                      "svr-%s", servers[j]);
 
-            if (settings.verbose > 1)
-                fprintf(stderr, "ping_test %s\n",
-                        server_key);
+            if (settings.verbose > 1) {
+                fprintf(stderr, "ping_test %s\n", detail_key);
+            }
 
             proxy_behavior behavior;
 
             memset(&behavior, 0, sizeof(behavior));
 
-            char **props = get_key_values(form, server_key);
+            char **props = get_key_values(form, detail_key);
             for (int k = 0; props && props[k]; k++) {
-                cproxy_parse_behavior_key_val_str(props[k],
-                                                  &behavior);
+                cproxy_parse_behavior_key_val_str(props[k], &behavior);
             }
 
-            ping_server(servers[j], latency_iterations,
+            ping_server(servers[j], recipes,
                         &behavior, opaque, add_report);
+        }
+
+        /* The recipe memory allocations */
+        for (int j = 0; j < nrecipes; j++) {
+            free(recipes[j].name);
         }
     }
 
     add_report(opaque, NULL, NULL);
 }
 
+static void perform_ping_test(struct ping_test_recipe recipe,
+                              memcached_st *mst,
+                              struct moxi_stats *out, int *failures)
+{
+    double *timing_results = calloc(recipe.iterations, sizeof(double));
+    assert(timing_results);
+
+    struct timeval timing = { 0, 0 };
+
+    char *key = calloc(recipe.keysize, sizeof(char));
+    assert(key);
+    char *value = calloc(recipe.valsize, sizeof(char));
+    assert(value);
+
+    /* Key is all 't's...just because */
+    memset(key, 't', recipe.keysize);
+    /* Value is a random bunch of stuff */
+    for (int i = 0; i < recipe.valsize; i++) {
+        value[i] = random() & 0xff;
+    }
+
+    if (memcached_set(mst,
+                      key, recipe.keysize,
+                      value, recipe.valsize,
+                      0, 0) != MEMCACHED_SUCCESS) {
+        /* XXX:  Holy crap!  Failure */
+    }
+
+    for (int i = 0 ; i < recipe.iterations; i++) {
+        struct timeval tv_pre = { 0, 0 } , tv_post = { 0, 0 };
+        size_t retrieved_len = 0;
+        uint32_t flags = 0;
+        memcached_return error;
+
+        gettimeofday(&tv_pre, NULL);
+
+        char *retrieved = memcached_get(mst,
+                                        key, recipe.keysize,
+                                        &retrieved_len, &flags,
+                                        &error);
+
+        gettimeofday(&tv_post, NULL);
+        timeval_subtract(&timing, &tv_post, &tv_pre);
+        timing_results[i] = timeval_to_double(timing);
+
+        if (retrieved) {
+            free(retrieved);
+        } else {
+            *failures++;
+        }
+    }
+
+    compute_stats(out, timing_results, recipe.iterations);
+    free(timing_results);
+    free(key);
+    free(value);
+}
+
 void ping_server(char *server_name,
-                 int latency_iterations,
+                 struct ping_test_recipe *recipes,
                  proxy_behavior *behavior,
                  void *opaque,
                  conflate_add_ping_report add_report) {
@@ -118,9 +246,19 @@ void ping_server(char *server_name,
     kvtmp->next = kvr;                          \
     kvr = kvtmp;
 
+#define int_report(name, ival)                  \
+    snprintf(buf, sizeof(buf), "%d", ival);     \
+    kvtmp = mk_kvpair(name, bufa);              \
+    kvtmp->next = kvr;                          \
+    kvr = kvtmp;
+
 #define tv_report(name, mark, val)                  \
     timeval_subtract(&timing, &val, &mark);         \
     dbl_report(name, timeval_to_double(timing));
+
+#define stat_report(buf, buflen, name, type, dval)  \
+    snprintf(buf, buflen, "%s_%s", name, type);     \
+    dbl_report(buf, dval);
 
     if (memcached_create(&mst) != NULL) {
         memcached_behavior_set(&mst, MEMCACHED_BEHAVIOR_NO_BLOCK, 1);
@@ -167,29 +305,27 @@ void ping_server(char *server_name,
             }
 
             if (connected) {
+                for (int j = 0; recipes[j].name; j++) {
+                    struct moxi_stats recipe_stats = { 0.0 };
+                    int failures = 0;
 
-                // TODO: Need a better ping test here.
-                // TODO: Set a few small & big values, and get them.
-                double v_results[latency_iterations];
-                for (int i = 0; i < latency_iterations; i++) {
-                    struct timeval tv_version_pre;
-                    gettimeofday(&tv_version_pre, NULL);
+                    perform_ping_test(recipes[j], &mst,
+                                      &recipe_stats, &failures);
+                    int vlen = strlen(recipes[j].name) + 8;
+                    char val_name[vlen];
 
-                    memcached_version(&mst);
+                    stat_report(val_name, vlen, recipes[j].name,
+                                "min", recipe_stats.min);
+                    stat_report(val_name, vlen, recipes[j].name,
+                                "avg", recipe_stats.avg);
+                    stat_report(val_name, vlen, recipes[j].name,
+                                "max", recipe_stats.max);
+                    stat_report(val_name, vlen, recipes[j].name,
+                                "stddev", recipe_stats.stddev);
 
-                    struct timeval tv_version_post;
-                    gettimeofday(&tv_version_post, NULL);
-                    timeval_subtract(&timing, &tv_version_post,
-                                     &tv_version_pre);
-                    v_results[i] = timeval_to_double(timing);
+                    snprintf(val_name, vlen, "%s_fail", recipes[j].name);
+                    int_report(val_name, failures);
                 }
-
-                struct moxi_stats version_stats = { 0.0 };
-                compute_stats(&version_stats, v_results, latency_iterations);
-                dbl_report("version_min", version_stats.min);
-                dbl_report("version_avg", version_stats.avg);
-                dbl_report("version_max", version_stats.max);
-                dbl_report("version_stddev", version_stats.stddev);
             }
         }
 
