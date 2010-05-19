903b21530bd4b6a34575273b5727e660041aeeeb
Steve Yen <steve.yen@gmail.com>
2010-05-19 12:38:50 -0700

    mcs.c added proper typed functions that replace mcs.h macros

--- mcs.h
+++ mcs.h
@@ -19,38 +19,48 @@ ssize_t memcached_io_write(memcached_server_st *ptr,
                            size_t length, char with_flush);
 void memcached_io_reset(memcached_server_st *ptr);
 memcached_return memcached_do(memcached_server_st *ptr,
-                              const void *commmand,
+                              const void *command,
                               size_t command_length,
                               uint8_t with_flush);
 
-// Some macros as level-of-indirection as opposed to direct
-// using libmemcached API.
+// The mcs API's are a level of indirection from direct libmemcached
+// API usage.
 //
-#define mcs_return memcached_return
-
+#define mcs_return    memcached_return
 #define mcs_st        memcached_st
 #define mcs_server_st memcached_server_st
 
-#define mcs_create(x)             memcached_create(x)
-#define mcs_free(x)               memcached_free(x)
-#define mcs_behavior_set(x, b, v) memcached_behavior_set(x, b, v)
-#define mcs_server_count(x)       memcached_server_count(x)
-#define mcs_server_push(x, s)     memcached_server_push(x, s)
-#define mcs_server_index(x, i)    (&((x)->hosts[(i)]))
-#define mcs_key_hash(x, k, len)   memcached_generate_hash(x, k, len)
+mcs_st *mcs_create(mcs_st *ptr);
+void    mcs_free(mcs_st *ptr);
 
-#define mcs_server_st_parse(str)  memcached_servers_parse(str)
-#define mcs_server_st_free(s)     memcached_server_list_free(s)
-#define mcs_server_st_quit(s, v)  memcached_quit_server(s, v)
+mcs_return mcs_behavior_set(mcs_st *ptr, memcached_behavior flag, uint64_t data);
 
-#define mcs_server_st_connect  memcached_connect
-#define mcs_server_st_do       memcached_do
-#define mcs_server_st_io_write memcached_io_write
-#define mcs_server_st_io_reset memcached_io_reset
-#define mcs_server_st_read     memcached_safe_read
+uint32_t       mcs_server_count(mcs_st *ptr);
+mcs_return     mcs_server_push(mcs_st *ptr, mcs_server_st *list);
+mcs_server_st *mcs_server_index(mcs_st *ptr, int i);
 
-#define mcs_server_st_hostname(s) ((s)->hostname)
-#define mcs_server_st_port(s)     ((s)->port)
-#define mcs_server_st_fd(s)       ((s)->fd)
+uint32_t mcs_key_hash(mcs_st *ptr, const char *key, size_t key_length);
+
+mcs_server_st *mcs_server_st_parse(const char *server_strings);
+void           mcs_server_st_free(mcs_server_st *ptr);
+void           mcs_server_st_quit(mcs_server_st *ptr, uint8_t io_death);
+
+mcs_return mcs_server_st_connect(mcs_server_st *ptr);
+mcs_return mcs_server_st_do(mcs_server_st *ptr,
+                            const void *commmand,
+                            size_t command_length,
+                            uint8_t with_flush);
+ssize_t mcs_server_st_io_write(mcs_server_st *ptr,
+                               const void *buffer,
+                               size_t length,
+                               char with_flush);
+mcs_return mcs_server_st_read(mcs_server_st *ptr,
+                              void *dta,
+                              size_t size);
+void mcs_server_st_io_reset(mcs_server_st *ptr);
+
+const char *mcs_server_st_hostname(mcs_server_st *ptr);
+int mcs_server_st_port(mcs_server_st *ptr);
+int mcs_server_st_fd(mcs_server_st *ptr);
 
 #endif // MCS_H
