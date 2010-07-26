b4413ecab37e289fad209225cf12d5a25e97e2e1
Steve Yen <steve.yen@gmail.com>
2010-07-26 09:38:52 -0700

    refactored ProxyClientBase into a separate reusable base class
    so that ascii and binary tests will have less repeated code
    
    Change-Id: I42d4575b10fefe016351d51fb629be95cb9aa59a
    Reviewed-on: http://review.northscale.com/1320
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- t/moxi_mock_server.py
+++ t/moxi_mock_server.py
@@ -121,9 +121,127 @@ class MockSession(threading.Thread):
 
 # Start a fake memcached server...
 #
-# sys.setcheckinterval(0)
-# g_mock_server_port = 11311
-# g_mock_server = MockServer(g_mock_server_port)
-# g_mock_server.start()
-# time.sleep(1)
+sys.setcheckinterval(0)
+g_mock_server_port = 11311
+g_mock_server = MockServer(g_mock_server_port)
+g_mock_server.start()
+time.sleep(1)
+
+class ProxyClientBase(unittest.TestCase):
+    vbucketId = 0
+
+    def __init__(self, x):
+        unittest.TestCase.__init__(self, x)
+
+        # These tests assume a moxi proxy is running at
+        # the self.proxy_port and is forwarding requests
+        # to our fake memcached server.
+        #
+        # TODO: Fork a moxi proxy like the perl tests.
+        #
+        self.proxy_port = 11333
+        self.clients = {}
+
+    def mock_server(self):
+        global g_mock_server
+        return g_mock_server
+
+    def setUp(self):
+        """setUp"""
+
+    def tearDown(self):
+        self.mock_close()
+        for k in self.clients:
+            self.client_close(k)
+        self.clients = []
+
+    def client_connect(self, idx=0):
+        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c.connect(("127.0.0.1", self.proxy_port))
+        self.clients[idx] = c
+        return c
+
+    def client_send(self, what, idx=0):
+        debug(1, "client sending " + what)
+        self.clients[idx].send(what)
+
+    def mock_send(self, what, session_idx=0):
+        debug(1, "mock sending " + what)
+
+        session = self.mock_server().sessions[session_idx]
+
+        self.assertTrue(session.client is not None)
+
+        session.client.send(what)
+
+    def client_recv(self, what, idx=0, num_bytes=1024):
+        debug(1, "client_recv expect: " + what)
+
+        s = self.clients[idx].recv(num_bytes)
+
+        debug(1, "client_recv actual: " + s);
+
+        self.assertTrue(what == s or re.match(what, s) is not None)
+
+    def mock_session(self, session_idx=0):
+        wait_max = 5
+
+        i = 1
+        while len(self.mock_server().sessions) <= session_idx and i < wait_max:
+            time.sleep(i)
+            i = i * 2
+
+        if len(self.mock_server().sessions) <= session_idx and i >= wait_max:
+            debug(1, "waiting too long for mock_session " + str(i))
+
+        return self.mock_server().sessions[session_idx]
+
+    def mock_recv_message(self, what, session_idx=0):
+        debug(1, "mock_recv expect: " + what)
+
+        session = self.mock_session(session_idx)
+
+        wait_max = 5
+        i = 1
+        while len(session.received) <= 0 and i < wait_max:
+            debug(1, "sleeping waiting for mock_recv " + str(i))
+            time.sleep(i)
+            i = i * 2
+
+        if len(session.received) <= 0 and i >= wait_max:
+            debug(1, "waiting too long for mock_recv " + str(i))
+
+        message = ""
+        if len(session.received) > 0:
+            message = session.received.pop(0)
+
+        debug(1, "mock_recv actual: " + message);
+
+        return message
+
+    def mock_recv(self, what, session_idx=0):
+        # Useful for ascii messages.
+        message = self.mock_recv_message(what, session_idx)
+        self.assertTrue(what == message or re.match(what, message) is not None)
+
+    def wait(self, x):
+        debug(1, "wait " + str(x))
+        time.sleep(0.01 * x)
+
+    def client_close(self, idx=0):
+        if self.clients[idx]:
+            self.clients[idx].close()
+        self.clients[idx] = None
+
+    def mock_close(self):
+        if self.mock_server():
+            self.mock_server().closeSessions()
+
+    def mock_quiet(self, session_idx=0):
+        if len(self.mock_server().sessions) <= session_idx:
+            return True
+
+        session = self.mock_server().sessions[session_idx]
+
+        return len(session.received) <= 0
 
